226a31f83cfa161fa471a4e0be5f3335238265dc
Carlos Eduardo Polidori Wendling <cadupolidori@gmail.com>
2024-11-01 14:52:16 -0300

    create buttons

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ color5 = '#FFAB40' # orange
 
 window = Tk()
 window.title('Calculator')
-window.geometry("235x318")
+window.geometry("235x310")
 window.config(bg=color1)
 
 display_frame = Frame(window, width=235, height=50, bg=color3)
@@ -18,5 +18,47 @@ display_frame.grid(row=0, column=0)
 
 body_frame = Frame(window, width=235, height=268)
 body_frame.grid(row=1, column=0)
+
+# buttons
+b_1 = Button(body_frame, text="C", width=11, height=2, bg=color4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_1.place(x=0, y=0)
+b_2 = Button(body_frame, text="%", width=5, height=2, bg=color4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_2.place(x=118, y=0)
+b_3 = Button(body_frame, text="/", width=5, height=2, bg=color5, fg=color2, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_3.place(x=177, y=0)
+
+b_4 = Button(body_frame, text="7", width=5, height=2, bg=color4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_4.place(x=0, y=52)
+b_5 = Button(body_frame, text="8", width=5, height=2, bg=color4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_5.place(x=59, y=52)
+b_6 = Button(body_frame, text="9", width=5, height=2, bg=color4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_6.place(x=118, y=52)
+b_7 = Button(body_frame, text="*", width=5, height=2, bg=color5, fg=color2, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_7.place(x=177, y=52)
+
+b_8 = Button(body_frame, text="4", width=5, height=2, bg=color4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_8.place(x=0, y=104)
+b_9 = Button(body_frame, text="5", width=5, height=2, bg=color4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_9.place(x=59, y=104)
+b_10 = Button(body_frame, text="6", width=5, height=2, bg=color4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_10.place(x=118, y=104)
+b_11 = Button(body_frame, text="-", width=5, height=2, bg=color5, fg=color2, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_11.place(x=177, y=104)
+
+b_12 = Button(body_frame, text="1", width=5, height=2, bg=color4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_12.place(x=0, y=156)
+b_13 = Button(body_frame, text="2", width=5, height=2, bg=color4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_13.place(x=59, y=156)
+b_14 = Button(body_frame, text="3", width=5, height=2, bg=color4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_14.place(x=118, y=156)
+b_15 = Button(body_frame, text="+", width=5, height=2, bg=color5, fg=color2, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_15.place(x=177, y=156)
+
+b_1 = Button(body_frame, text="0", width=11, height=2, bg=color4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_1.place(x=0, y=208)
+b_2 = Button(body_frame, text=".", width=5, height=2, bg=color4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_2.place(x=118, y=208)
+b_3 = Button(body_frame, text="=", width=5, height=2, bg=color5, fg=color2, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_3.place(x=177, y=208)
 
 window.mainloop()
